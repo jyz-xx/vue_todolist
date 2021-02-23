2c5e38dc8fb142f1d859a65cff2b7214b65267e2
jyz-xx <215774124@qq.com>
2021-02-23 20:50:55 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,35 @@
 <template>
   <div id="app">
-    <a-input placeholder="请输入任务" class="my_ipt" />
-    <a-button type="primary">添加事项</a-button>
+    <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="handleInputChange"/>
+    <a-button type="primary" @click="addItemToList">添加事项</a-button>
 
-    <a-list bordered :dataSource="list" class="dt_list">
+    <a-list bordered :dataSource="infolist" class="dt_list">
       <a-list-item slot="renderItem" slot-scope="item">
         <!-- 复选框 -->
-        <a-checkbox>{{ item.info }}</a-checkbox>
+        <a-checkbox :checked="item.done" @change="(e) => {cbstatusChanged(e, item.id)}">{{ item.info }}</a-checkbox>
         <!-- 删除链接 -->
-        <a slot="actions">删除</a>
+        <a slot="actions" @click="removeItemById(item.id)">删除</a>
       </a-list-item>
 
       <!-- footer区域 -->
       <div slot="footer" class="footer">
         <!-- 未完成的任务个数 -->
-        <span>0条剩余</span>
+        <span>{{ unDoneLength }}条剩余</span>
         <!-- 操作按钮 -->
         <a-button-group>
-          <a-button type="primary">全部</a-button>
-          <a-button>未完成</a-button>
-          <a-button>已完成</a-button>
+          <a-button :type="viewKey === 'all'? 'primary' : 'default' " @click="changelist('all')">全部</a-button>
+          <a-button :type="viewKey === 'undone'? 'primary' : 'default' " @click="changelist('undone')">未完成</a-button>
+          <a-button :type="viewKey === 'done'? 'primary' : 'default' " @click="changelist('done')">已全部</a-button>
         </a-button-group>
         <!-- 把已经完成的任务清空 -->
-        <a>清除已完成</a>
+        <a @click="clean">清除已完成</a>
       </div>
     </a-list>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 export default {
   name: 'app',
   data() {
@@ -43,10 +43,36 @@ export default {
     handleInputChange(e) {
       // console.log(e.target.value)
       this.$store.commit('setInputValue', e.target.value)
+    },
+    addItemToList() {
+      if (this.inputValue.trim().length <= 0) {
+        return this.$message.warning('文本框内容不能为空！')
+      }
+      this.$store.commit('addItem')
+    },
+    // 根据id删除对应的任务事项
+    removeItemById(id) {
+      // console.log(id)
+      this.$store.commit('removeItem', id)
+    },
+    cbstatusChanged(e, id) {
+      const param = {
+        id: id,
+        status: e.target.checked
+      }
+      this.$store.commit('changeStatus', param)
+    },
+    clean() {
+      this.$store.commit('cleanDone')
+    },
+    changelist(key) {
+      // console.log(key);
+      this.$store.commit('changeViewKey', key)
     }
   },
   computed: {
-    ...mapState(['list', 'inputValue'])
+    ...mapState(['inputValue', 'viewKey']),
+    ...mapGetters(['unDoneLength', 'infolist'])
   }
 }
 </script>
